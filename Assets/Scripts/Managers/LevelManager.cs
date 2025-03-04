e76e65d5897cbe4adaa6d7df7abfca9c9f174b25
munim khaliq <97252132+munim-k@users.noreply.github.com>
2025-03-05 00:00:29 +0500

    save game state retrieve not working

--- Assets/Scripts/Managers/LevelManager.cs
+++ Assets/Scripts/Managers/LevelManager.cs
@@ -1,4 +1,8 @@
 using UnityEngine;
+using Unity.Services.CloudSave;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
 
 public class LevelManager : MonoBehaviour
 {
@@ -9,7 +13,131 @@ public class LevelManager : MonoBehaviour
     public bool[] levelsUnlocked = new bool[LEVELS];
     public bool[] levelsCompleted = new bool[LEVELS];
 
+    
+
     public int level;
+
+
+     public async Task LoadGameData()
+    {
+        try
+        {
+        (levelsUnlocked, levelsCompleted) = await LoadLevelData(LEVELS);
+        }
+
+        catch (System.Exception ex)
+        {
+            Debug.LogError($"Error loading level data: {ex.Message}");
+        }
+        for (int i = 0; i < LEVELS; i++)
+        {
+            Debug.Log($"Level {i + 1}: Unlocked = {levelsUnlocked[i]}, Completed = {levelsCompleted[i]}");
+        }
+    }
+
+    public static async Task SaveLevelData(bool[] levelsUnlocked, bool[] levelsCompleted)
+    {
+        try
+        {
+            
+            List<int> unlockedList = new List<int>();
+            List<int> completedList = new List<int>();
+
+            foreach (bool unlocked in levelsUnlocked)
+                unlockedList.Add(unlocked ? 1 : 0);
+
+            foreach (bool completed in levelsCompleted)
+                completedList.Add(completed ? 1 : 0);
+
+            
+            await CloudSaveService.Instance.Data.Player.SaveAsync(new Dictionary<string, object>
+            {
+                { "levels_unlocked", unlockedList },
+                { "levels_completed", completedList }
+            });
+
+            Debug.Log("Level data saved to Cloud Save.");
+        }
+        catch (System.Exception ex)
+        {
+            Debug.LogError($"Error saving level data: {ex.Message}");
+        }
+    }
+
+    public static async Task<(bool[], bool[])> LoadLevelData(int LEVELS)
+    {
+        try
+        {
+            var savedData = await CloudSaveService.Instance.Data.Player.LoadAsync(new HashSet<string> { "levels_unlocked", "levels_completed" });
+
+            bool[] levelsUnlocked = new bool[LEVELS];
+            bool[] levelsCompleted = new bool[LEVELS];
+
+            
+            if (savedData.TryGetValue("levels_unlocked", out var unlockedObj))
+            {
+                
+                List<int> unlockedList = DeserializeList(unlockedObj);
+                for (int i = 0; i < Mathf.Min(unlockedList.Count, LEVELS); i++)
+                    if( unlockedList[i] == 1)
+                        levelsUnlocked[i] = true;
+            }
+
+            
+            if (savedData.TryGetValue("levels_completed", out var completedObj))
+            {
+                
+                List<int> completedList = DeserializeList(completedObj);
+                
+                for (int i = 0; i < Mathf.Min(completedList.Count, LEVELS); i++)
+                     if(completedList[i] == 1)
+                        levelsCompleted[i] = true;
+            }
+
+            Debug.Log("Level data loaded from Cloud Save.");
+            return (levelsUnlocked, levelsCompleted);
+        }
+        catch (System.Exception ex)
+        {
+            Debug.LogError($"Error loading level data: {ex.Message}");
+            return (new bool[LEVELS], new bool[LEVELS]); 
+        }
+    }
+
+    private static List<int> DeserializeList(object data)
+    {
+        try
+        {
+            if (data is IList<object> objectList)
+            {
+                List<int> intList = new List<int>();
+                foreach (var item in objectList)
+                    intList.Add(System.Convert.ToInt32(item));
+                return intList;
+            }
+            else if (data is string jsonString)
+            {
+                return JsonConvert.DeserializeObject<List<int>>(jsonString) ?? new List<int>();
+            }
+            else if (data is int singleValue)
+            {
+                return new List<int> { singleValue };
+            }
+        }
+        catch (System.Exception ex)
+        {
+            Debug.LogError($"Error deserializing list: {ex.Message}");
+        }
+
+        return new List<int>();
+    }
+
+
+    private async Task saveData()
+    {
+        await SaveLevelData(levelsUnlocked, levelsCompleted);
+    }
+
 
     private void Awake()
     {
@@ -24,16 +152,28 @@ public class LevelManager : MonoBehaviour
 
         DontDestroyOnLoad(gameObject);
     }
+    private void Start()
+    {
+        InitializeGameData();
+    }
+    private async Task InitializeGameData()
+    {
+        await LoadGameData();
+    }
 
-    public void UnlockLevel(int level)
+    public async Task UnlockLevel(int level)
     {
         levelsUnlocked[level] = true;
+        await saveData();
         
     }
 
-    public void CompleteLevel(int level)
+    public async Task CompleteLevel(int level)
     {
         levelsCompleted[level] = true;
+        await saveData();
     }
+
+   
 
 }
