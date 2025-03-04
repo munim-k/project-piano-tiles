7f8d6b97c891ca39d678de1e64b73974d4e0ff6c
Muhammad Tayyab <141912893+CodeBard-x88@users.noreply.github.com>
2025-03-04 23:37:37 +0500

    third web sdk v4.7.5 testing

--- Assets/Scripts/Web3/ConnectWallet.cs
+++ Assets/Scripts/Web3/ConnectWallet.cs
@@ -2,26 +2,63 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using Thirdweb;
+using System;
+using System.Numerics;
+using System.Threading.Tasks;
 
-namespace Thirdweb.Unity{
-public class ConnectWallet : MonoBehaviour
+namespace Thirdweb.Unity
 {
-    private IThirdwebWallet userAddress;
+    public class ConnectWallet : MonoBehaviour
+    {
+        private string _address;
+        private ChainData _currentChainData;
 
-    public async void onClick(){
-        var options = new WalletOptions(provider: WalletProvider.MetaMaskWallet, chainId: 592);
-        var wallet = await ThirdwebManager.Instance.ConnectWallet(options);
-
-        if (wallet != null)
+        private void Start()
         {
-            userAddress = wallet;
-            SceneManager.LoadScene("Loading"); 
-        }
-        else
-        {
-            Debug.LogError("Wallet Connection Failed!");
+            _currentChainData = ThirdwebManager.Instance.supportedChains.Find(x => x.identifier == ThirdwebManager.Instance.activeChain);
         }
 
+        public async void onClick()
+        {
+            var wc = new WalletConnection(provider: WalletProvider.WalletConnect, chainId: BigInteger.Parse(_currentChainData.chainId));
+            await Connect(wc);
+        }
+
+        private async Task Connect(WalletConnection wc)
+        {
+            Debug.Log($"Connecting to {wc.provider}...");
+
+            await Task.Delay(500);
+
+            try
+            {
+                _address = await ThirdwebManager.Instance.SDK.wallet.Connect(wc);
+                if (!string.IsNullOrEmpty(_address))
+                {
+                    PostConnect();
+                }
+                else
+                {
+                    Debug.LogError("Wallet Connection Failed!");
+                }
+            }
+            catch (Exception e)
+            {
+                _address = null;
+                Debug.LogError($"Failed to connect: {e}");
+            }
+        }
+
+        private async void PostConnect()
+        {
+            Debug.Log($"Connected to {_address}");
+
+            var bal = await ThirdwebManager.Instance.SDK.wallet.GetBalance();
+            var balStr = $"{bal.value.ToEth()} {bal.symbol}";
+            Debug.Log($"Balance: {balStr}");
+
+            SceneManager.LoadScene("Loading");
+        }
     }
-}
 }
